578107787d83349cce6d15904cb2e9ddb94ba70d
cyyllam <cyyllam@gmail.com>
2025-07-01 14:56:32 -0700

    flextable images can only be used with rmd

--- render.R
+++ render.R
@@ -1,18 +1,32 @@
-library(quarto)
 library(tidyverse)
+library(rmarkdown)
+library(quarto)
 
 # create vector of centers
-centers <- read.csv("C:\\Users\\CLam\\github\\centers-monitoring\\data\\centers_information.csv") |> 
+centers <- read.csv("C:\\Users\\CLam\\github\\centers-monitoring\\data\\centers_information.csv") |>
   filter(name %in% c("SeaTac", "Renton", "Bellevue Downtown")) |>
-  # head(2) |> 
-  distinct(name) |> 
-  pull(name) |> 
+  # head(2) |>
+  distinct(name) |>
+  pull(name) |>
   as.character()
 
+# using quarto ----
+
+# reports <- tibble(
+#   input = "report.qmd",
+#   output_file = str_glue("{centers}.docx"),
+#   execute_params = map(centers, ~list(center = .))
+# )
+# 
+# pwalk(reports, quarto_render)
+
+
+# using rmarkdown ----
+
 reports <- tibble(
-  input = "report.qmd",
+  input = "report.Rmd",
   output_file = str_glue("{centers}.docx"),
-  execute_params = map(centers, ~list(center = .))
+  params = map(centers, ~list(center = .))
 )
 
-pwalk(reports, quarto_render)
+pwalk(reports, ~rmarkdown::render(input = ..1, output_file = ..2, params = ..3))
